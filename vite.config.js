575ba91c7c2fadaf6b8e8c22f565051a30cec672
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2025-09-03 20:39:48 +0300

    fix styles

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/weatherApp/",
-  plugins: [vitePluginHttps()],
-  server: {
-    https: true
-  }
+  // plugins: [vitePluginHttps()],
+  // server: {
+  //   https: true
+  // }
 });
